1ae71ef076bbeec77ecfe64d0837b216af0607da
Geisson <60269749+Geisson19@users.noreply.github.com>
2022-01-25 15:53:47 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://dbAdmin:<password8)>@clusterisis.qewpv.mongodb.net/dbUniappto?"
+app.config["MONGO_URI"] = "mongodb+srv://???????"
 mongo = PyMongo(app)
 
 CORS(app)
